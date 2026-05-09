a71d9f271714e2d539c21c8654d5a0ef3cc0eff4
zaundamathews <bed-com-27-24@unima.ac.mw>
2026-05-09 15:37:12 +0200

    cleaned admin

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,15 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AdminController } from './admin.controller';
-import { AdminService } from './admin.service';
-import { User } from '../users/entities/user.entity';
-import { Claim } from '../lost-found/entities/claim.entity';
-import { LostFoundItem } from '../lost-found/entities/lost-found-item.entity';
-import { Announcement } from '../announcements/entities/announcement.entity';
 
-@Module({
-  imports: [TypeOrmModule.forFeature([User, Claim, LostFoundItem, Announcement])],
-  controllers: [AdminController],
-  providers: [AdminService],
-})
+@Module({})
 export class AdminModule {}
